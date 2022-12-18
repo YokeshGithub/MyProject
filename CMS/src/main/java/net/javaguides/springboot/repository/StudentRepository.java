2420c53df4ee32451943b78a70f656aa31170357
Yokesh <111746788+YokeshGithub@users.noreply.github.com>
2022-12-18 09:44:45 +0530

    Delete CMS directory

--- CMS/src/main/java/net/javaguides/springboot/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package net.javaguides.springboot.repository;
-import net.javaguides.springboot.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
